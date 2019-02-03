207dab7db1e76d19c42ed4b1664446a83c6e250c
sunny-selector-0188 <sonya_dumitru@yahoo.com>
2019-02-03 21:01:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,31 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ //we create an empty object
+ var price = Math.floor(Math.random() * 99 + 1);
+ var itemsObject = {};
+ intemsObject[item] = price;
+ cart.push(intemsObject);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  for (let i=0; i < cart.length; i++) {
+    if (cart.length === 0) {
+      console.log("Your shopping cart is empty.");
+    } else {
+
+    }
+  }
 }
 
 function total() {
   // write your code here
+  for (let i=0; i < cart.length; i++) {
+
+  }
 }
 
 function removeFromCart(item) {
